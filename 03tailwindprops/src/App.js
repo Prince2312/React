6dd97f4fc8b04de4f27e7a0258a31f659ba96676
Prince2312 <149226198+Prince2312@users.noreply.github.com>
2025-04-29 06:05:49 +0000

    props and tailwind

---
+++ 03tailwindprops/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-green-400">
+      Hello world!
+    </h1>
+    <Card/>
+    </>
+  );
+}
+
+export default App;
